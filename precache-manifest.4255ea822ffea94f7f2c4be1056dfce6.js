5311748a0b6cf194f176a4bca73597d518332468
pjonp <pjonp01@gmail.com>
2019-12-31 05:30:07 -0500

    Updates

---
+++ precache-manifest.4255ea822ffea94f7f2c4be1056dfce6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cac92789167c0b94707cef5d91bbdd9",
+    "url": "/profile-image-watermarker/index.html"
+  },
+  {
+    "revision": "9551dd5bf7bbed628b04",
+    "url": "/profile-image-watermarker/static/css/2.6ede3073.chunk.css"
+  },
+  {
+    "revision": "97b682ee8e469b390931",
+    "url": "/profile-image-watermarker/static/css/main.6d4d6e32.chunk.css"
+  },
+  {
+    "revision": "9551dd5bf7bbed628b04",
+    "url": "/profile-image-watermarker/static/js/2.3b547b20.chunk.js"
+  },
+  {
+    "revision": "b9026fa6b3f0581974371c32c336717b",
+    "url": "/profile-image-watermarker/static/js/2.3b547b20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97b682ee8e469b390931",
+    "url": "/profile-image-watermarker/static/js/main.3e0cbdd6.chunk.js"
+  },
+  {
+    "revision": "9d74eac33dee8ca2e891",
+    "url": "/profile-image-watermarker/static/js/runtime-main.f493a2b4.js"
+  }
+]);
